ff84f445ccdcf62e3ab7d0b165cef7901d1ba832
shun-k1331 <shunkmc13de@gmail.com>
2014-04-07 00:35:58 +0900

    dotfiles to change Dotfiles

--- set_dotfiles_slink.sh
+++ set_dotfiles_slink.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DOTFILES=`find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`
+DOTFILES=`find $HOME/Dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`
 
 
 for file in ${DOTFILES[@]}
 do
 	rm -f $HOME/$file
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $HOME/Dotfiles/$file $HOME/$file
 done
